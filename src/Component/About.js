75013fd0c0ae8322ad2cf7c0a9d76c9d12707924
PRATIMASINGH123 <pratima16singh123@gmail.com>
2022-10-01 21:30:40 +0530

    react

--- src/Component/About.js
+++ src/Component/About.js
@@ -7,13 +7,14 @@ function About()
                    <h1>ABOUT ME</h1>
                 </div>
                 <div className="intro">
-                    <h1>i am pratima</h1>
+                    <h1>I'm <strong>Pratima Singh</strong>, FULL STACK WEB DEVELOPER</h1>
                     <h2>I am 22,live in pune. I have completed my graduation
                         in <strong>BCA</strong> in the academic year 2021 and after that
-                        i have joined NS for learning more web development while
+                        i have devloped my interest joined NS for learning more web development while
                         pursuing this course I have made/created which I have 
-                        mentioned in me resume which are  . Apart from
-                        that I fancy Dancing,singing,sketching,traveling,ETC.</h2>
+                        mentioned in me resume which are . Apart from
+                        that I fancy Dancing,singing,sketching,traveling.
+                        i </h2>
                 </div>
                 </div>
     )
